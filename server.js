019dc9b123463ef8d4276d08905a15dece8e2727
Sean Doyle <sdoylelambda@gmail.com>
2019-05-20 18:57:16 -0400

    Get by id CRUD method.

--- server.js
+++ server.js
@@ -16,4 +16,18 @@ server.get('/', async (req, res) => {
         }
 });
 
+server.get('/:id', async(req, res) =>{
+    try {
+        const data = await Data.findById(req.params.id);
+        if (data) {
+            res.json(data);
+        } else {
+            res.status(404).json({ msg: 'Data not found.'})
+        }
+    } catch (error) {
+        console.log(error) 
+            res.status(500).json({ msg: 'The ID does not exist.'});
+    }
+});
+
 module.exports = server;
