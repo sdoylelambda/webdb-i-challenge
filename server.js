94326a9df392f1bd541d527e3dd3431b488ea83a
Sean Doyle <sdoylelambda@gmail.com>
2019-05-20 18:53:24 -0400

    Get method.

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const server = express();
 
-// your code here
+const Data = require('./data/accounts-model');
+
+server.use(express.json());
+
+server.get('/', async (req, res) => {
+        try {
+            const data = await Data.find();
+            res.json(data);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: 'The data could not be found.'});
+        }
+});
 
 module.exports = server;
